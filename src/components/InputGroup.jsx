75c51323f95fc1d036ee26af045afe3a03e3af2e
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-03-06 23:03:23 +0300

    Add InputGroup component for flexible form inputs

---
+++ src/components/InputGroup.jsx
@@ -0,0 +1,40 @@
+export default function InputGroup({
+    type,
+    id,
+    labelText,
+    placeholder,
+    value,
+    onChange,
+    "data-key": dataKey,
+    recommended,
+    optional,
+}) {
+    return (
+        <div className="input-group">
+            <label htmlFor={id}>
+                <span className="label-text">{labelText}</span>
+                {optional && <span className="optional-text">optional</span>}
+                {recommended && <span className="recommended-text">recommended</span>}
+            </label>
+            
+            {type === "Textarea" ? (
+                <textarea
+                id={id}
+                placeholder={placeholder}
+                onChange={onChange}
+                value={value}
+                data-key={dataKey}
+                ></textarea>
+            ) : (
+                <input
+                type={type}
+                id={id}
+                placeholder={placeholder}
+                onChange={onChange}
+                value={value}
+                data-key={dataKey}
+                />
+            )}
+        </div>
+    )
+}
