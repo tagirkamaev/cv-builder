49277e88de80cd824e9a3f8a8320bb1434be1c2e
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-03-06 23:14:06 +0300

    Add EducationInfo component for displaying education details

---
+++ src/components/education/EducationInfo.jsx
@@ -0,0 +1,22 @@
+import '../../styles/EducationInfo.css'
+
+export default function EducationInfo({ info }) {
+    const { schoolName, degree, location, startDate, endDate } = info
+    return (
+        <div className="education-info">
+            <div className="education-info-group">
+                <p className="dates">
+                    {startDate}
+                    {startDate && endDate && <span> - </span>}
+                    {endDate}
+                </p>
+                <p>{location}</p>
+            </div>
+
+            <div className="education-info-group">
+                <p className="education-info-schoolName">{schoolName}</p>
+                <p className="education-info-degree">{degree}</p>
+            </div>
+        </div>
+    )
+}
