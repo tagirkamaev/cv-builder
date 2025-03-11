a267b9fdf489dd58a2847211f9bdc037180e4280
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-03-11 10:18:20 +0300

    Refactor App.jsx to create resume builder main component
    - Replaced default Vite React template with resume builder application
    - Integrated components for personal details, education, and experience sections
    - Added state management for form data and section interactions
    - Implemented functionality to create, modify, and manage resume sections
    - Removed default App.css and updated import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,184 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
+import PersonalDetails from './components/personal-info/PersonalDetails'
+import AddEducationSection from './components/education/AddEducationSection'
+import AddExperienceSection from './components/experience/AddExperienceSection'
+import Resume from './components/Resume'
+import uniqid from 'uniqid'
+import TemplateLoader from './components/TemplateLoader'
+import exampleData from './example-data'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo)
+  const [sections, setSections] = useState(exampleData.sections)
+  const [sectionOpen, setSectionOpen] = useState(null)
+  const [currentPage, setCurrentPage] = useState('content')
+  const [resumeLayout, setResumeLayout] = useState('top')
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [prevState, setPrevState] = useState(null)
+
+  function handlePersonalInfoChange(e) {
+    const {key} = e.target.dataset;
+    setPersonalInfo({...personalInfo, [key]: e.target.value })
+  }
+
+  function handleSectionChange(e) {
+    const { key } = e.target.dataset
+    const inputValue = e.target.value
+    const form = e.target.closest('.section-form')
+    const { id } = form
+    const { arrayName } = form.dataset
+    const section = sections[arrayName]
+    setSections({
+      ...sections,
+      [arrayName]: section.map((obj) => {
+        if (obj.id === id) obj[key] = inputValue
+        return obj
+      })
+    })
+  }
+
+  function createForm(arrayName, object) {
+    setPrevState(null)
+
+    const section = structuredClone(sections[arrayName])
+    section.push(object)
+    setSections({ ...sections, [arrayName]: section })
+  }
+  
+  
+  const createEducationForm = () => 
+    createForm('educations', {
+      degree: '',
+      schoolName: '',
+      location: '',
+      startDate: '',
+      endDate: '',
+      isCollapsed: false,
+      isHidden: false,
+      id: uniqid(),
+    })
+
+  const createExperienceForm = () => 
+    createForm('experiences', {
+      companyName: '',
+      positionTitle: '',
+      location: '',
+      description: '',
+      startDate: '',
+      endDate: '',
+      isCollapsed: false,
+      isHidden: false,
+      id: uniqid(),
+    })
+
+    const setOpen = (sectionName) => setSectionOpen(sectionName)
+
+    function removeForm(e) {
+      const form = e.target.closest('.section-form')
+      const { arrayName } = form.dataset
+      const section = sections[arrayName]
+      const { id } = form
+
+      setSections({
+        ...sections,
+        [arrayName]: section.filter((item) => item.id !== id),
+      })
+    }
+
+    function cancelForm(e) {
+      if (prevState === null) {
+        removeForm(e)
+        return
+      }
+    }
+
+    function toggleValue(e, key) {
+      const sectionForm = e.target.closest('.section-form')
+      const { id } = sectionForm
+      const { arrayName } = sectionForm.dataset
+      const section = sections[arrayName]
+      setSections({
+        ...sections,
+        [arrayName]: section.map((form) => {
+          if (form.id === id) {
+            setPrevState(Object.assign({}, form))
+            form[key] = !form[key]
+          }
+
+          return form
+        })
+      })
+    }
+
+    const toggleCollapsed = (e) => toggleValue(e, 'isCollapsed')
+    const toggleHidden = (e) => toggleValue(e, 'isHidden')
+
+    return (
+      <div className='app'>
+        <div className='edit-side'>
+          <Sidebar onGoToPage={setCurrentPage} page={currentPage} />
+          <div className='form-container'>
+            <TemplateLoader
+              onTemplateLoad={() => {
+                setPersonalInfo(exampleData.personalInfo)
+                setSections(exampleData.sections)
+              }}
+              onClear={() => {
+                setPersonalInfo({
+                  fullName: '',
+                  email: '',
+                  phoneNumber: '',
+                  address: '',
+                })
+                setSections({ educations: [], experiences: [] })
+                setPrevState(null)
+              }}
+            />
+            {currentPage === 'content' && (
+              <>
+                <PersonalDetails
+                  onChange={handlePersonalInfoChange}
+                  fullName={personalInfo.fullName}
+                  email={personalInfo.email}
+                  phoneNumber={personalInfo.phoneNumber}
+                  address={personalInfo.address}
+                /> 
+                <AddEducationSection
+                  educations={sections.educations}
+                  isOpen={sectionOpen === 'Education'}
+                  onChange={handleSectionChange}
+                  createForm={createEducationForm}
+                  setOpen={setOpen}
+                  onCancel={cancelForm}
+                  toggleCollapsed={toggleCollapsed}
+                  onHide={toggleHidden}
+                  onRemove={removeForm}
+                />
+                <AddExperienceSection
+                  experiences={sections.experiences}
+                  isOpen={sectionOpen === 'Experience'}
+                  onChange={handleSectionChange}
+                  createForm={createExperienceForm}
+                  setOpen={setOpen}
+                  onCancel={cancelForm}
+                  toggleCollapsed={toggleCollapsed}
+                  onHide={toggleHidden}
+                  onRemove={removeForm}
+                />
+              </>
+            )}
+          </div>
+        </div>
+
+        <Resume
+          personalInfo={personalInfo}
+          sections={sections}
+          layout={resumeLayout}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
 export default App
