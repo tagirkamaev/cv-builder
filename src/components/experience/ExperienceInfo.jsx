9a6549c9c0c482d083a0048e65dc70585c361ffd
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-03-11 10:08:09 +0300

    Added ExperienceInfo component with styling for displaying work experience details

---
+++ src/components/experience/ExperienceInfo.jsx
@@ -0,0 +1,24 @@
+import '../../styles/ExperienceInfo.css'
+
+export default function ExperienceInfo({ info }) {
+    const { companyName, positionTitle, location, description, startDate, endDate } = info
+
+    return (
+        <div className="experience-info">
+            <div className="experience-info-group">
+                <p className="dates">
+                    {startDate}
+                    {startDate && endDate && <span> - </span>}
+                    {endDate}
+                </p>
+                <p>{location}</p>
+            </div>
+
+            <div className="experience-info-group">
+                <p className="experience-info-companyName">{companyName}</p>
+                <p className="experience-info-positionTitle">{positionTitle}</p>
+                <p className="experience-info-description">{description}</p>
+            </div>
+        </div>
+    )
+}
