90b18705d883e24b8e13f118ca160cf0f700505b
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-03-12 11:58:29 +0300

    Add Prettier configuration and formatting script
    - Added .prettierignore to exclude build and dependency directories
    - Created .prettierrc with consistent code formatting rules
    - Added Prettier as a dev dependency in package.json
    - Included npm script for formatting source files
    - Applied Prettier formatting to existing components and stylesheets

--- src/components/Customize.jsx
+++ src/components/Customize.jsx
@@ -1,23 +1,23 @@
-import "../styles/customize/Customize.css";
-import AccentColorEdit from "./customize/AccentColorEdit";
-import FontEdit from "./customize/FontEdit";
-import ResumeColsEdit from "./customize/ResumeColsEdit";
+import '../styles/customize/Customize.css'
+import AccentColorEdit from './customize/AccentColorEdit'
+import FontEdit from './customize/FontEdit'
+import ResumeColsEdit from './customize/ResumeColsEdit'
 
 export default function Customize({ isShown, onColChange }) {
-    return (
-        <div
-            className="customize"
-            style={{ display: isShown ? undefined : 'none'}}
-        >
-            <div>
-                <ResumeColsEdit onColChange={onColChange} />
-            </div>
-            <div>
-                <AccentColorEdit />
-            </div>
-            <div>
-                <FontEdit />
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className="customize"
+      style={{ display: isShown ? undefined : 'none' }}
+    >
+      <div>
+        <ResumeColsEdit onColChange={onColChange} />
+      </div>
+      <div>
+        <AccentColorEdit />
+      </div>
+      <div>
+        <FontEdit />
+      </div>
+    </div>
+  )
 }
